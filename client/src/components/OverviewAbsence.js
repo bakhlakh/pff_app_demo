0e6d9c0ba6db1fa7f74e119d6da6e37a73e78594
Nouh Bakhlakh <nouhbk@gmail.com>
2022-06-09 23:39:05 +0000

    chart absence

---
+++ client/src/components/OverviewAbsence.js
@@ -0,0 +1,201 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { Chart } from "react-chartjs-2";
+import "chart.js/auto";
+import { Chart as ChartJS, Tooltip } from "chart.js";
+
+const OverviewAbsence = React.memo((props) => {
+  Tooltip.positioners.custom = (elements, eventPosition) => {
+    return {
+      x: eventPosition.x,
+      y: eventPosition.y,
+    };
+  };
+  const [selectedView, setSelectedView] = useState("1j");
+  const [chartData, setChartData] = useState(props.data[0]);
+  const [legend, setLegend] = useState([]);
+  useEffect(() => {
+    const newData = props.data.filter((obj) => obj.type === selectedView)[0];
+    setChartData(newData);
+  }, [selectedView]);
+  const [ref, setRef] = useState(null);
+  const onRefChange = useCallback(
+    (props) => {
+      setRef(props);
+      return props;
+    },
+    [chartData]
+  );
+  useEffect(() => {
+    if (ref) beforeInit(ref);
+  }, [ref, chartData]);
+
+  const beforeInit = (chart) => {
+    let datasetsLabels = [];
+    let colors = [];
+    chart.data.datasets.forEach((ds) => {
+      datasetsLabels.push({
+        label: ds.label,
+      });
+      colors.push(ds.backgroundColor);
+    });
+    setLegend(
+      <ul className="LegendListStyle">
+        {datasetsLabels &&
+          datasetsLabels.map((obj, index) => (
+            <li key={index} style={{ textAlign: "left" }}>
+              <div className="DashboardTournamentsOverviewPanel-legend">
+                <span style={{ backgroundColor: colors[index] }}></span>
+                <h4>{obj.label} </h4>
+              </div>
+            </li>
+          ))}
+      </ul>
+    );
+  };
+  return (
+    <div className="DashboardTournamentsOverviewPanel">
+      <div className="DashboardTournamentsOverviewPanel-header">
+        <div className="DashboardTournamentsOverviewPanel-header-item">
+          <h5>Resume des absences</h5>
+        </div>
+        <div className="DashboardTournamentsOverviewPanel-header-item">
+          <ul className="dropdown-options">
+            <li
+              className={"option" + (selectedView === "1j" ? " selected" : "")}
+              onClick={() => {
+                setSelectedView("1j");
+              }}
+            >
+              1j
+            </li>
+
+            <li
+              className={"option" + (selectedView === "1m" ? " selected" : "")}
+              onClick={() => {
+                setSelectedView("1m");
+              }}
+            >
+              1m
+            </li>
+            <li
+              className={"option" + (selectedView === "1y" ? " selected" : "")}
+              onClick={() => {
+                setSelectedView("1y");
+              }}
+            >
+              1y
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="DashboardTournamentsOverviewPanel-legend-container">
+        {legend}
+      </div>
+      <Chart
+        ref={onRefChange}
+        data={chartData}
+        type="line"
+        style={{ maxHeight: "180px", margin: "5px" }}
+        options={{
+          responsive: true,
+          scales: {
+            y: {
+              ticks: {
+                display: false,
+                max: 25000,
+                min: 0,
+                stepSize: 5000,
+                beginAtZero: true,
+                padding: 20,
+                callback: function (value) {
+                  return value / 1000 + "k";
+                },
+              },
+              grid: {
+                borderWidth: 0,
+                display: false,
+              },
+            },
+            x: {
+              ticks: {
+                display: true,
+                font: {
+                  family: "Poppins",
+                  size: 12,
+                  weight: 400,
+                  style: "normal",
+                  lineHeight: 1.5,
+                },
+                color: "#333333",
+              },
+
+              grid: {
+                display: false,
+                borderWidth: 0,
+              },
+            },
+          },
+
+          plugins: {
+            legend: {
+              display: false,
+              position: "top",
+              labels: {
+                usePointStyle: true,
+                pointStyle: "circle",
+                boxWidth: 5,
+                font: {
+                  size: 15,
+                  lineHeight: 100,
+                  weight: 400,
+                  style: "normal",
+                  family: "Poppins",
+                },
+                color: "#7F85A2",
+              },
+            },
+            tooltip: {
+              displayColors: false,
+              enabled: true,
+              backgroundColor: "#F2F5FF",
+              yAlign: "bottom",
+              position: "custom",
+              intersect: true,
+              titleColor: ({ tooltipItems }) => {
+                return tooltipItems[0]?.dataset?.backgroundColor;
+              },
+              bodyColor: (tooltip) => {
+                if (tooltip.tooltip) {
+                  return tooltip.tooltip.labelTextColors[0];
+                }
+              },
+              titleAlign: "center",
+              bodyAlign: "center",
+              titleFont: {
+                family: "Poppins",
+                size: 15,
+                weight: "bold",
+                style: "normal",
+                lineHeight: 1.5,
+              },
+              titleMarginBottom: 1,
+              callbacks: {
+                label: function (tooltipItem) {
+                  return tooltipItem.dataset.label;
+                },
+                title: (tooltipItem) => {
+                  return tooltipItem[0].dataset.data[tooltipItem[0].dataIndex];
+                },
+              },
+            },
+            datalabels: {
+              display: false,
+            },
+          },
+        }}
+      />
+    </div>
+  );
+});
+
+export default OverviewAbsence;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useStoreActions } from "easy-peasy";
 
 import { DashboardCard } from "../components/DashboardCard";
+import OverviewAbsence from "../components/OverviewAbsence";
 
 import "../styles/componentStyles/dashboardCard.css";
 import "../styles/pagesStyles/DashboardStyle.css";
@@ -12,10 +13,128 @@ import ExtensionIcon from "@mui/icons-material/Extension";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import GroupsIcon from "@mui/icons-material/Groups";
 
+import Grid from "@mui/material/Grid";
+
 function Home() {
   const getOverview = useStoreActions((actions) => actions.getOverview);
   const [overview, setOverview] = useState([]);
+  const tournamentsData = [
+    {
+      labels: ["08:30", "11:00", "13:30", "16:00"],
+      datasets: [
+        {
+          label: "Justifier",
+          data: [11, 12, 23, 34],
+          backgroundColor: "#62CA76",
+          borderColor: "#62CA76",
+          pointRadius: 0,
+          tension: 0.1,
+          borderWidth: 1,
+          pointHoverBackgroundColor: "white",
 
+          pointHoverBorderWidth: 4,
+          pointHoverRadius: 10,
+        },
+        {
+          label: "Non justifier",
+          data: [34, 11, 23, 12],
+          backgroundColor: "#4569FF",
+          borderColor: "#4569FF",
+          pointRadius: 0,
+          tension: 0.1,
+          borderWidth: 1,
+          pointHoverBackgroundColor: "white",
+
+          pointHoverBorderWidth: 4,
+          pointHoverRadius: 10,
+        },
+      ],
+      type: "1j",
+    },
+    {
+      labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+      datasets: [
+        {
+          label: "Justifier",
+          data: [1230, 1301, 1109, 1002, 1793, 2903, 4093],
+          total: 7092,
+          backgroundColor: "#4569FF",
+          borderColor: "#4569FF",
+          pointRadius: 0,
+          tension: 0.1,
+          borderWidth: 1,
+          pointHoverBorderWidth: 4,
+          pointHoverBackgroundColor: "white",
+          pointHoverRadius: 10,
+        },
+        {
+          label: "Non justifier",
+          data: [34, 123, 435, 634, 565, 123, 534, 787, 987, 774, 456, 984],
+          total: "0.23%",
+          backgroundColor: "#62CA76",
+          borderColor: "#62CA76",
+          pointRadius: 0,
+          tension: 0.1,
+          borderWidth: 1,
+          pointHoverBorderWidth: 4,
+          pointHoverBackgroundColor: "white",
+          pointHoverRadius: 10,
+        },
+      ],
+      type: "1m",
+    },
+    {
+      labels: [
+        "Janvier",
+        "Fevrier",
+        "Mars",
+        "Avril",
+        "May",
+        "Juin",
+        "Juillet",
+        "Aout",
+        "Septembre",
+        "Octobre",
+        "Novembre",
+        "Decembre",
+      ],
+      datasets: [
+        {
+          label: "Justifier",
+          data: [
+            99384, 109345, 143345, 239384, 229384, 319384, 301384, 230384,
+            193184, 173084, 143084, 193184,
+          ],
+          total: 34213,
+          backgroundColor: "#4569FF",
+          borderColor: "#4569FF",
+          pointRadius: 0,
+          tension: 0.1,
+          borderWidth: 1,
+          pointHoverBorderWidth: 4,
+          pointHoverBackgroundColor: "white",
+          pointHoverRadius: 10,
+        },
+        {
+          label: "Non Justifier",
+          data: [
+            9938, 10345, 13345, 39384, 29384, 39334, 33384, 23384, 19318, 13084,
+            14308, 19184,
+          ],
+          total: "0.59%",
+          backgroundColor: "#62CA76",
+          borderColor: "#62CA76",
+          pointRadius: 0,
+          tension: 0.1,
+          borderWidth: 1,
+          pointHoverBackgroundColor: "white",
+          pointHoverBorderWidth: 4,
+          pointHoverRadius: 10,
+        },
+      ],
+      type: "1y",
+    },
+  ];
   const fetchData = async () => {
     const result = await getOverview();
     if (result) {
@@ -57,7 +176,14 @@ function Home() {
   }, []);
   return (
     <>
-      <DashboardHeader headerData={overview} />
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={12}>
+          <DashboardHeader headerData={overview} />
+        </Grid>
+        <Grid item xs={6}>
+          <OverviewAbsence data={tournamentsData} />
+        </Grid>
+      </Grid>
     </>
   );
 }
